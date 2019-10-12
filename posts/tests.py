fe9a941e1fae24d2fa93ecc8fc5a37be4315dc61
Bishop216 <44731013+Bishop216@users.noreply.github.com>
2019-10-12 15:29:04 +0300

    Added some unit tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,1 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from .views import home, createpost
+
+
+class ViewTests(TestCase):
+
+    def setUp(self):
+        self.request_factory = RequestFactory()
+        self.user = get_user_model().objects.create_user(email='test@mail.ru', password='test213', first_name='Jack',
+                                                         last_name='Jack')
+
+    def test_post(self):
+        request = self.request_factory.post(reverse(createpost), {'title': 'test', 'content': 'test'})
+        request.user = self.user
+        response = createpost(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home(self):
+        request = self.request_factory.get(reverse(home))
+        response = home(request)
+        self.assertEqual(response.status_code, 200)
+
+
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import User
+
+
+class ModelTest(TestCase):
+
+    def setUp(self):
+        user = User(email='test@mail.ru', password='test213', first_name='Jack', last_name='Jack')
+        user.save()
+        user = User.objects.get(email='test@mail.ru')
+        print(user)
 
 
