f1c3fef6086be0fdd36974205de1872c6d6f245f
Bishop216 <44731013+Bishop216@users.noreply.github.com>
2019-10-12 15:49:41 +0300

    Added unit test for create_user endpoint

--- user/tests.py
+++ user/tests.py
@@ -2,14 +2,8 @@ from django.test import TestCase, RequestFactory
 from django.contrib.auth import get_user_model
 from . import views
 from django.urls import reverse
+from django.contrib.auth.models import AnonymousUser
 
-
-class ModelTest(TestCase):
-
-    def user_creation(self):
-        self.request_factory = RequestFactory()
-        self.user = get_user_model().objects.create_user(email='test@mail.ru', password='test213', first_name='Jack',
-                                                         last_name='Jack')
 
 class ViewTest(TestCase):
 
@@ -18,10 +12,15 @@ class ViewTest(TestCase):
         self.user = get_user_model().objects.create_user(email='test@mail.ru', password='test213', first_name='Jack',
                                                          last_name='Jack')
 
+    def test_create_user(self):
+        request = self.request_factory.post(reverse(views.CreateUserAPIView.as_view()),
+                                            {'email': 'test2@mail.ru', 'password': 'test123', 'first_name': 'John',
+                                             'last_name': 'John'})
+        response = views.CreateUserAPIView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+
     def test_authenticate(self):
         request = self.request_factory.post(reverse(views.authenticate_user))
         request.user = self.user
         response = views.authenticate_user(request)
         self.assertEqual(response.status_code, 200)
-
-
