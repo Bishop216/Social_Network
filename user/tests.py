d9023f4b45acf2602681c7fbd64bc6ab57d424e5
Bishop216 <44731013+Bishop216@users.noreply.github.com>
2019-10-12 15:41:08 +0300

    Added unit test for user views

--- user/tests.py
+++ user/tests.py
@@ -1,13 +1,27 @@
-from django.test import TestCase
-from .models import User
+from django.test import TestCase, RequestFactory
+from django.contrib.auth import get_user_model
+from . import views
+from django.urls import reverse
 
 
 class ModelTest(TestCase):
 
+    def user_creation(self):
+        self.request_factory = RequestFactory()
+        self.user = get_user_model().objects.create_user(email='test@mail.ru', password='test213', first_name='Jack',
+                                                         last_name='Jack')
+
+class ViewTest(TestCase):
+
     def setUp(self):
-        user = User(email='test@mail.ru', password='test213', first_name='Jack', last_name='Jack')
-        user.save()
-        user = User.objects.get(email='test@mail.ru')
-        print(user)
+        self.request_factory = RequestFactory()
+        self.user = get_user_model().objects.create_user(email='test@mail.ru', password='test213', first_name='Jack',
+                                                         last_name='Jack')
+
+    def test_authenticate(self):
+        request = self.request_factory.post(reverse(views.authenticate_user))
+        request.user = self.user
+        response = views.authenticate_user(request)
+        self.assertEqual(response.status_code, 200)
 
 
